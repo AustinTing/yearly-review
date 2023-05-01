300d151d67d05e705157ab2fd825264b6874b099
CellBody <austintingwork@gmail.com>
2023-05-01 21:42:52 +0800

    feature: test nivo

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // For Nivo Calendar to work, see: https://github.com/plouc/nivo/issues/1941#issuecomment-1067197724
+  experimental: {
+    esmExternals: 'loose'
+  }
 }
 
 module.exports = nextConfig
